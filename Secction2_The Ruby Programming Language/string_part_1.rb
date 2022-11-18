59db7a8ddae59deeb1c5bcb4542f3b6d19823943
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-18 18:28:39 -0300

    Update string_part_1.rb

--- Secction2_The Ruby Programming Language/string_part_1.rb
+++ Secction2_The Ruby Programming Language/string_part_1.rb
@@ -5,3 +5,25 @@ puts first_name + " " + last_name
 
 #String interpolation
 puts "\nMy first name is #{first_name}, and my last name is #{last_name}"
+
+#How to find a method
+#p first_name.methods
+
+#Some comum methods
+puts"\n"
+p 10.class
+p 10.to_s.class #changing class (chain method)
+p first_name.length #space included
+p first_name.reverse
+p first_name.empty?
+p "".empty?
+puts"\n"
+
+#Replace words
+sentence = "Welcome to the jungle!!!"
+p sentence
+p sentence.sub("the jungle", "utopia") #replace
+ 
+#Scape seguence
+p "My fisrt name is \#{first_name}"
+
