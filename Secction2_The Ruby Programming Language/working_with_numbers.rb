abde3abc30d250b68c9e424ac0184c22ea6381a4
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-19 11:25:48 -0300

    Create working_with_numbers.rb

---
+++ Secction2_The Ruby Programming Language/working_with_numbers.rb
@@ -0,0 +1,23 @@
+#Numbers
+p 1+5
+p 5*2
+p 10/2
+p 10/4 #Float??
+p 10.0 /4 #Float number
+p 10/4.0
+p 10/4.to_f #Every numbers is float
+
+puts"\n"
+
+puts "Fist nine"
+puts "="*20
+puts "Second line"
+20.times{print"="}
+
+puts"\n"
+
+5.times{puts "Hello!"}
+
+puts"\n"
+
+10.times{puts rand(5)} #Random numbers generator (last number not included)
