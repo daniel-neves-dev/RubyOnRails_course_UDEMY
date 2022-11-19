d842dc9b31c7834e81c544c08f3af06d1ad25bbb
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-19 12:42:32 -0300

    using function (method)

--- Secction2_The Ruby Programming Language/working_with_numbers_simple_calculator.rb
+++ Secction2_The Ruby Programming Language/working_with_numbers_simple_calculator.rb
@@ -1,3 +1,23 @@
+def sum(n1,n2)
+    return n1+n2.round(2)
+end
+
+def subtraction (n1,n2)
+    return n1-n2.round(2)
+end
+
+def multiply (n1,n2)
+     return n1*n2.round(2)
+end
+
+def division (n1,n2)
+    return n1/n2.round(2)
+end
+
+def modulus(n1,n2)
+    return n1%n2
+end
+
 18.times{print "="}
 puts
 puts "SIMPLE CALCULATOR"
@@ -8,8 +28,8 @@ n1 = gets.chomp.to_f
 puts"Enter the second number:"
 n2 = gets.chomp.to_f
 
-puts "#{n1} + #{n2} = #{n1 + n2}"
-puts "#{n1} - #{n2} = #{n1 - n2}"
-puts "#{n1} x #{n2} = #{n1 * n2}"
-puts "#{n1} / #{n2} = #{n1 / n2}"
+puts "#{n1} + #{n2} = #{sum(n1,n2)}"
+puts "#{n1} - #{n2} = #{subtraction(n1,n2)}"
+puts "#{n1} * #{n2} = #{multiply(n1,n2)}"
+puts "#{n1} / #{n2} = #{modulus(n1,n2)}"
 puts "#{n1} division remaind #{n2} = #{n1 % n2}"
