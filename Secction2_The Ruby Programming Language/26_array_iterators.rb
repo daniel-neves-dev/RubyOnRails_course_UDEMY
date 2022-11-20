cd3256710a68aed390fc2da944b0ee461e511d60
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-20 16:21:35 -0300

    Create 26_array_iterators.rb

---
+++ Secction2_The Ruby Programming Language/26_array_iterators.rb
@@ -0,0 +1,40 @@
+a = [1,2,3,4,5,6,7,8,9]
+p a
+puts a
+puts
+p a.last #last array item
+
+#RANGE method (..)
+r = 1..50
+p r
+p r.to_a #array
+puts
+p r.to_a.shuffle #random
+
+puts
+#alphabet
+alphabet = "a".."z"
+p alphabet.to_a
+p alphabet.to_a.reverse
+p alphabet.to_a.length
+
+#Adding elements
+puts
+p a.length
+a << 10 #adding last element
+p a
+p a.last
+a.unshift("Daniel")
+p a.first
+p a
+a << "Daniel"
+p a
+a.uniq! #ereaser same tipe (! = permanet)
+p a
+
+puts
+p a.include?("Daniel")
+p a.include?("Oliveira")
+
+#PUSH include new item
+p a.push("new item")
