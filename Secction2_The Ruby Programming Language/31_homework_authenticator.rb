9e82192ab6550cfe3a53f9749b6390a3210e325d
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-21 17:20:24 -0300

    testing condition

--- Secction2_The Ruby Programming Language/31_homework_authenticator.rb
+++ Secction2_The Ruby Programming Language/31_homework_authenticator.rb
@@ -1,10 +1,27 @@
 users = [
-    {username: "daniel", password: "password1"}
+    {username: "daniel", password: "password1"},
     {username: "paula", password: "password2"}
 ]
-
+puts
 puts "Welcome to the authenticator"
 25.times{print "*"}
 puts
 puts "This program will take input from the user and compare password"
 puts "If password is correct, you will get back the user object"
+
+puts
+puts "Enter your user name:"
+user_name = gets.chomp
+puts "Enter your password:"
+user_password = gets.chomp
+
+puts
+users.each do |user|
+    if user[:username] == user_name && user[:password] == user_password
+        puts user
+        break      
+    end
+end
+puts "Credentials were not correct"
+
+
