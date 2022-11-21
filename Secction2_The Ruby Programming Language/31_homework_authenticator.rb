8fdb01043112874274100428444ab25e7044c52c
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-21 16:43:56 -0300

    Create 31_homework_authenticator.rb

---
+++ Secction2_The Ruby Programming Language/31_homework_authenticator.rb
@@ -0,0 +1,10 @@
+users = [
+    {username: "daniel", password: "password1"}
+    {username: "paula", password: "password2"}
+]
+
+puts "Welcome to the authenticator"
+25.times{print "*"}
+puts
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
