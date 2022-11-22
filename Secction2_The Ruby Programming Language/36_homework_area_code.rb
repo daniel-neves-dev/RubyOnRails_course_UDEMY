fe373ccaa9586ef4408bcf73ab1d71fe19f03c27
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-22 13:42:13 -0300

    Update 36_homework_area_code.rb

--- Secction2_The Ruby Programming Language/36_homework_area_code.rb
+++ Secction2_The Ruby Programming Language/36_homework_area_code.rb
@@ -10,3 +10,26 @@ dial_book = {
   "Orlando" => "407",
   "Lancaster" => "717"
 }
+
+def get_city_names(dial_book)
+  dial_book.keys
+end
+
+def area_code(city, key)
+  city[key]
+end
+
+loop do
+  print "\nDo you want to verify a city area code? (Y/N):"
+  prompt = gets.chomp.downcase
+  break if prompt!="y"
+  puts get_city_names(dial_book)
+  puts "\nWhat city do you want to verify?"
+  city = gets.chomp
+  if dial_book.include?(city)
+    puts "The area code for #{city} is: #{area_code(dial_book, city)}"
+  else
+    puts
+    puts "There is no city with this name in the list."
+  end
+end
