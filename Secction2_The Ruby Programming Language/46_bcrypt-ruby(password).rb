b69039a4495e2f6c023a5d254c23651107f71221
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-23 11:37:31 -0300

    BCrypt_version_1.1
    List of users
    display password

--- Secction2_The Ruby Programming Language/46_bcrypt-ruby(password).rb
+++ Secction2_The Ruby Programming Language/46_bcrypt-ruby(password).rb
@@ -3,11 +3,28 @@
 require 'bcrypt'
 
 #Example
+users = [
+	{username: "daniel", password: "password1"},
+	{username: "paula", password: "password2"},
+	{username: "ricardo", password: "password3"},
+  {username: "carla", password: "password4"}
+]
+
+#Creating new password
 def create_digtal_password(password)
   BCrypt::Password.create(password) #BCrypt method
 end
 
-new_password = create_digtal_password("password1")
-puts new_password
-puts new_password == "password1"
-puts new_password == "password2"
+#Password verification
+def verify_digital_password(password)
+  BCrypt::Password.new(password) #BCrypt method
+end
+
+#Call users list
+def create_secure_users(list_users)
+  list_users.each do |user_record|#variavel
+    puts user_record[:password]#from the list
+  end
+end
+
+create_secure_users(users)
