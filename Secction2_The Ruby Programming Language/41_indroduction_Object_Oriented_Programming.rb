c0c7b53f937dc57f09ff620de31db5de688936e8
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-23 10:10:38 -0300

    new vesion_1.1
    add - attr_reader

--- Secction2_The Ruby Programming Language/41_indroduction_Object_Oriented_Programming.rb
+++ Secction2_The Ruby Programming Language/41_indroduction_Object_Oriented_Programming.rb
@@ -2,13 +2,17 @@
 class Student
   #attributes(@something)
   #to access the attributes (attr_accessor)
-  attr_accessor :first_name,:last_name, :email, :username #class identities
+  attr_accessor :first_name,:last_name, :email #class identities
+  attr_reader :username #only to read a identit
   @first_name
   @last_name
   @email
   @username
   @password
 
+  def set_username
+    @username = "daniel1"
+  end
   
 end
   
@@ -16,7 +20,7 @@ daniel = Student.new
 daniel.first_name = "Daniel"
 daniel.last_name = "Oliveira"
 daniel.email = "daniel@exemplo.com"
-daniel.username = "daniel1"
+daniel.set_username
 
 puts daniel.first_name 
 puts daniel.last_name
