4c7cfbe731db379da504a4ecc75459a806b57ba2
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-23 10:38:43 -0300

    final_version
    def initialize()
    def to_s

--- Secction2_The Ruby Programming Language/41_indroduction_Object_Oriented_Programming.rb
+++ Secction2_The Ruby Programming Language/41_indroduction_Object_Oriented_Programming.rb
@@ -2,27 +2,25 @@
 class Student
   #attributes(@something)
   #to access the attributes (attr_accessor)
-  attr_accessor :first_name,:last_name, :email #class identities
-  attr_reader :username #only to read a identit
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  attr_accessor :first_name,:last_name, :email, :password #class identities
+  
 
-  def set_username
-    @username = "daniel1"
+  def initialize(first_name, last_name, username, email, password) #the name must be 'initialize'
+    @first_name = first_name #instance variable
+    @last_name = last_name
+    @username = username
+    @email = email
+    @password = password
   end
-  
-end
-  
-daniel = Student.new
-daniel.first_name = "Daniel"
-daniel.last_name = "Oliveira"
-daniel.email = "daniel@exemplo.com"
-daniel.set_username
 
-puts daniel.first_name 
-puts daniel.last_name
-puts daniel.email 
-puts daniel.username 
+  def to_s #'display' information. Must be 'to_s'
+    "Fisrt name: #{@first_name}, Last name: #{@last_name}, User name: #{@username}, Email: #{@email}, Password: #{@password}"
+  end
+end  
+daniel = Student.new("Daniel", "Oliveira", "daniel1", "daniel@exemplo.com", "password1")
+paula = Student.new("Paula", "Arosio", "paula11", "paula@exemplo.com", "password2")
+puts
+
+puts daniel
+puts paula
+puts
